31c6f403863f42d70b0b3b890d7c7754a3f3e67d
Aodacat <798953242@qq.com>
2020-02-17 16:15:27 +0800

    添加了动态读取profile配置文件机制

--- src/main/java/com/mycom/ssmdemo/SsmdemoApplication.java
+++ src/main/java/com/mycom/ssmdemo/SsmdemoApplication.java
@@ -3,7 +3,6 @@ package com.mycom.ssmdemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling       // 开启基于注解的定时任务
